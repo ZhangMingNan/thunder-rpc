f41d142069e0a3a6c15da03d195a30f1812ef111
zhangmingnan <origin_zmn@163.com>
2017-12-27 10:47:21 +0800

    完成序列化器注册中心.

--- src/main/java/com/ly/zmn48644/rpc/serializer/SerializerEngine.java
+++ src/main/java/com/ly/zmn48644/rpc/serializer/SerializerEngine.java
@@ -14,7 +14,17 @@ public class SerializerEngine {
     }
 
     //序列化
-
+    public byte[] serialize(Object object, SerializerType serializerType) {
+        //获取序列化器
+        ISerializer iSerializer = serializerMap.get(serializerType);
+        //调用序列化方法
+        return iSerializer.serialize(object);
+    }
 
     //反序列化
+    public <T> T deserialize(byte[] data, Class<T> clazz,SerializerType serializerType) {
+        ISerializer iSerializer = serializerMap.get(serializerType);
+        return iSerializer.deserialize(data,clazz);
+    }
+
 }
