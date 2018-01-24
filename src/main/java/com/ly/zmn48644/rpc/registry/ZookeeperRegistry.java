972820549bd5a226b32e4be5b24730c977af435f
zhangmingnan <origin_zmn@163.com>
2018-01-24 22:59:12 +0800

    从zookeeper中获取服务列表

--- src/main/java/com/ly/zmn48644/rpc/registry/ZookeeperRegistry.java
+++ src/main/java/com/ly/zmn48644/rpc/registry/ZookeeperRegistry.java
@@ -8,12 +8,17 @@ import java.util.List;
  * 作者:张明楠(1007350771@qq.com)
  */
 public class ZookeeperRegistry {
+    private static final String ROOT = "/thunder";
+    private static final String PROVIDER_TYPE = "provider";
+    private static final String INVOKER_TYPE = "invoker";
 
     //zk客户端
     private ZkClient zkClient;
 
     public ZookeeperRegistry(ZkClient zkClient) {
         this.zkClient = zkClient;
+        //创建根节点
+        zkClient.createPersistent(ROOT);
     }
 
     /**
@@ -26,8 +31,18 @@ public class ZookeeperRegistry {
     /**
      * 向注册中心注册服务提供者信息
      */
-    public void registerProvider(List<Provider> providerList) {
-
+    public void registerProvider(Provider provider) {
+        //获取到提供者列表
+        String host = provider.getHost();
+        int port = provider.getPort();
+        String service = provider.getService();
+        String address = host + ":" + port;
+        String path = ROOT + "/" + service + "/" + PROVIDER_TYPE;
+        if (!zkClient.exists(path)) {
+            zkClient.writeData(path, address);
+        } else {
+            zkClient.createPersistent(path, address);
+        }
     }
 
     /**
